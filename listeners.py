92c9e58735a4d6d155ffcfc2ac077ab195ff6187
GotoCode <khalid.peer233@gmail.com>
2018-06-12 15:00:17 +0300

    Implement custom StreamListener for writing to MongoDB

--- listeners.py
+++ listeners.py
@@ -1,12 +1,16 @@
 from tweepy import StreamListener
-from pymongo import MongoClient
+from databases import MongoDB
 
 class StdOutListener(StreamListener):
 
     def on_status(self, status):
-
         print(status.text)
 
 class MongoDBListener(StreamListener):
 
-    pass
+    def __init__(self):
+        super().__init__()
+        self.driver = MongoDB()
+
+    def on_status(self, status):
+        self.driver.add_tweet(status)
