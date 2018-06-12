faa8b84a6e5fa3c38c84baa037d82633d43d9810
GotoCode <khalid.peer233@gmail.com>
2018-06-12 13:15:02 +0300

    Added authentication code to main tweet crawling logic

--- crawler.py
+++ crawler.py
@@ -7,15 +7,26 @@ from listeners import StdOutListener, MongoDBListener
 from databases import MongoDB
 
 class TweetCrawler:
-    def __init__(self):
+    
+    def __init__(self, auth):
+        self.auth = auth
+        self.api = tweepy.API(auth)
         self.db = MongoDB()
-        pass
 
     def crawl_tweets(self):
         pass
 
 def main():
-    crawler = TweetCrawler()
+    # retrieve the crawler's configuration options
+    with open('config.json') as f:
+        config = json.load(f)
+
+    # authenticate the crawler to access the Twitter API
+    auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
+    auth.set_access_token(config['access_token'], config['access_token_secret'])
+
+    # create and run the tweet crawler
+    crawler = TweetCrawler(auth)
     crawler.crawl_tweets()
 
 if __name__ == '__main__':
