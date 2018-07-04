80efcca02d440b1dfd1b0e327a7070ac52e17214
GotoCode <khalid.peer233@gmail.com>
2018-07-04 13:45:47 +0300

    Add new non-filtered streaming tweet crawler
    This commit adds a new type of streaming crawler
    which can listen to tweets streamed from the full
    Twitter firehose (without any filter criteria). In
    addition to providing more data, this crawler can
    be used to sample trends across the Twitter platform.

---
+++ sampler.py
@@ -0,0 +1,57 @@
+from urllib3.exceptions import ProtocolError
+import sys
+
+import tweepy
+
+from listeners import StdOutListener, MongoDBListener
+from databases import MongoDB
+from config import config
+
+# "random sample" tweet crawler
+class TweetSampler:
+
+    def __init__(self, auth):
+        self.auth = auth
+        self.api = tweepy.API(auth)
+        self.db = MongoDB()
+
+    def crawl_tweets(self, languages=None):
+        
+        main_listener = MongoDBListener(verbose=True)
+        languages = languages or []
+        
+        while True:
+            
+            try:
+                main_stream = tweepy.Stream(auth=self.auth, listener=main_listener)
+
+                # start crawling all tweets in the given language(s)
+                # NOTE: This is a BLOCKING call!
+                main_stream.sample(languages=languages)
+
+            except ProtocolError:
+                continue
+
+# driver for "random sample" crawler
+def main():
+    # authenticate the crawler to access the Twitter API
+    auth = tweepy.OAuthHandler(config['consumer_key'],
+                               config['consumer_secret'])
+
+    auth.set_access_token(config['access_token'],
+                          config['access_token_secret'])
+
+    # create and run the tweet sampler for the given languages
+    sampler = TweetSampler(auth)
+    langs = sys.argv[1:]
+    print('Listening for random sample of tweets in languages: {}'.format(langs), flush=True)
+
+    # gracefully handle Ctrl^C exit from tweet sampling process
+    try:
+        sampler.crawl_tweets(languages=langs)
+    except KeyboardInterrupt:
+        print('\nStopped tweet sampler for tweets in languages: {}'.format(langs))
+        
+if __name__ == '__main__':
+
+    main()
